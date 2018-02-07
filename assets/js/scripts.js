c97f3613adde38bbe2c55019d04d5bfcb0902613
KMS\anhpnguyen <anhpnguyen@kms-technology.com>
2018-02-07 14:23:01 +0700

    room

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,4 +7,20 @@ $(function () {
     }
     createDatepicker("#checkin");
     createDatepicker("#checkout");
+
+    $('#rooms').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '#rooms-nav'
+    });
+    $('#rooms-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '#rooms',
+        centerMode: true,
+        arrows: false,
+        focusOnSelect: true
+    });
 });

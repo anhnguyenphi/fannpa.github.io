5bd5204b5d688b408a3442334195b25b35e2e63f
KMS\anhpnguyen <anhpnguyen@kms-technology.com>
2018-02-26 15:00:43 +0700

    booking

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -23,4 +23,11 @@ $(function () {
         arrows: false,
         focusOnSelect: true
     });
+
+    $("#booking-slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+    });
 });
